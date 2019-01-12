f8bc5d04f4c925f7fcdc09023bce9d1921c67a3c
pyhblacksky <664254320@qq.com>
2019-01-12 14:49:01 +0800

    试采用velocity

--- src/main/java/com/nowcoder/controller/IndexController.java
+++ src/main/java/com/nowcoder/controller/IndexController.java
@@ -1,18 +1,30 @@
 package com.nowcoder.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping(path = {"/","/index"})    //默认访问/
-    @ResponseBody   //返回一个字符串，所以用Body
+    @RequestMapping(path = {"/","/index"}, method = {RequestMethod.GET, RequestMethod.POST})    //访问的路径
+    @ResponseBody   //返回一个页面
     public String index(){
         return "Hello world!";
     }
 
+    @RequestMapping(path = {"/profile/{groupId}/{userId}"})
+    @ResponseBody
+    public String profile(@PathVariable("userId") int userId,
+                          @PathVariable("groupId") String groupId,
+                          @RequestParam(value = "type",defaultValue = "0") int type,
+                          @RequestParam(value = "key",defaultValue = "none") String key){
+        return String.format("Profile Page of %s / %d, t: %d, key : %s", groupId,userId,type,key);
+    }
 
+    //返回一个模板
+    @RequestMapping(path = {"/vm"})
+    public String template(){
+        return "home";//返回一个home的模板
+    }
 
 }
