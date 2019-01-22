9eb10693091d7ad0b750a7ca91e6d3e7b867b4a5
pyhblacksky <664254320@qq.com>
2019-01-22 09:47:54 +0800

    修复显示问题

--- src/main/java/com/nowcoder/controller/HomeController.java
+++ src/main/java/com/nowcoder/controller/HomeController.java
@@ -1,7 +1,8 @@
 package com.nowcoder.controller;
 
-import com.nowcoder.model.Question;
-import com.nowcoder.model.ViewObject;
+import com.nowcoder.model.*;
+import com.nowcoder.service.CommentService;
+import com.nowcoder.service.FollowService;
 import com.nowcoder.service.QuestionService;
 import com.nowcoder.service.UserService;
 import org.slf4j.Logger;
@@ -28,10 +29,34 @@ public class HomeController {
     @Autowired
     QuestionService questionService;
 
+    @Autowired
+    CommentService commentService;
+
+    @Autowired
+    FollowService followService;
+
+    @Autowired
+    HostHolder hostHolder;
+
+    //返回用户页
     @RequestMapping(path = {"/user/{userId}"}, method = {RequestMethod.GET, RequestMethod.POST})
     public String userIndex(Model model, @PathVariable("userId") int userId) {
         model.addAttribute("vos", getQuestions(userId, 0, 10));
-        return "index";
+
+        User user = userService.getUser(userId);
+        ViewObject vo = new ViewObject();
+        vo.set("user", user);
+        vo.set("commentCount", commentService.getUserCommentCount(userId));
+        vo.set("followerCount", followService.getFollowerCount(EntityType.ENTITY_USER, userId));
+        vo.set("followeeCount", followService.getFolloweeCount(userId, EntityType.ENTITY_USER));
+        if(hostHolder.getUser() != null){
+            vo.set("followed", followService.isFollower(hostHolder.getUser().getId(), EntityType.ENTITY_USER, userId));
+        } else {
+            vo.set("followed", false);
+        }
+
+        model.addAttribute("profileUser", vo);
+        return "profile";
     }
 
 
@@ -48,6 +73,7 @@ public class HomeController {
         for(Question question : questionList){
             ViewObject vo = new ViewObject();
             vo.set("question", question);
+            vo.set("followCount", followService.getFollowerCount(EntityType.ENTITY_QUESTION, question.getId()));
             vo.set("user", userService.getUser(question.getUserId()));//获取的是用户id！！
             vos.add(vo);
         }
